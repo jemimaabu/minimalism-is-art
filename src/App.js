6377720a554d73b974d62851d64452c25878385f
Jemima Abu <jemimaabu@gmail.com>
2019-02-26 21:48:15 +0100

    Included active route

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './styles/App.css';
 import { Gallery } from './pages/GalleryPage';
 import { About } from './pages/AboutPage';
 import { Contact } from './pages/ContactPage'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 import { ParallaxEffect } from './components/ParallaxEffect/ParallaxEffect'
+
+const NoMatch = ({ location }) => (
+  <div>
+    The <b>{location.pathname}</b> page doesn't exist.
+  </div>
+);
 
 class App extends Component {
   render() {
@@ -15,26 +21,28 @@ class App extends Component {
             <h1>Minimalism<br/>Is Art</h1>
             <hr />
           </header>
-          <Router>
+          <BrowserRouter>
             <div>
               <nav className="page-navigation">
                 <li>
-                  <Link to="/">Gallery</Link>
+                  <NavLink to="/gallery">Gallery</NavLink>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <NavLink to="/about">About</NavLink>
                 </li>
                 <li>
-                  <Link to="/contact">Contact</Link>
+                  <NavLink to="/contact">Contact</NavLink>
                 </li>
               </nav>
-              <div  className="page-content__body">
+              <Switch>
                 <Route exact path="/" component={Gallery} />
+                <Route path="/gallery" component={Gallery} />
                 <Route path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
-              </div>
+                <Route component={NoMatch} />
+              </Switch>
             </div>
-          </Router>
+          </BrowserRouter>
         </main>
         <ParallaxEffect
             parallaxBackgroundImage = "https://66.media.tumblr.com/6d884fe09d90b1bac096df349d7b7b38/tumblr_orxj4oc6AH1uu9fqho1_500.gif"
